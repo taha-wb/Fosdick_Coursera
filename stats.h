d0aa83c7bae849e88164a97e5644138dfd59903a
MT <saed.taha02@gmail.com>
2025-05-12 13:02:45 +0300

    secomd commit with functions stubbed

---
+++ stats.h
@@ -0,0 +1,81 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <Add File Name> 
+ * @brief <Add Brief Description Here >
+ *
+ * <Add Extended Description Here>
+ *
+ * @author <Add FirsName LastName>
+ * @date <Add date >
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+
+// A function that prints the statistics of an array including minimum, maximum, mean, and median.
+
+
+void print_statistics(unsigned char *array, unsigned int length){
+    
+};
+
+
+// Given an array of data and a length, prints the array to the screen
+
+void print_array(unsigned char *array){};
+
+
+// Given an array of data and a length, returns the median value
+float find_median(unsigned char *array, unsigned int length){};
+
+
+
+// Given an array of data and a length, returns the mean
+float find_mean(unsigned char *array, unsigned int length){};
+
+
+// Given an array of data and a length, returns the maximum
+float find_maximum(unsigned char *array, unsigned int length){};
+
+// Given an array of data and a length, returns the minimum
+float find_minimum(unsigned char *array, unsigned int length){};
+
+
+// Given an array of data and a length, sorts the array from largest to smallest. 
+// (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
+void sort_array(unsigned char *array, unsigned int length){};
+
+
+
+
+
+
+
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
